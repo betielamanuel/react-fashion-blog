f4f44e06e337f646d38fa70393c26f75093432f2
John Doe <betiaman@example.com>
2023-07-27 09:11:20 -0500

    fashion blog rendering in react

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import Header from './Header';
 import Nav from './Nav';
 import Article from './Article';
 import Footer from './Footer';
-import './index.css'
+import './index.css';
+
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Nav />
-      <Article />
-      <Footer />
+        <Header />
+        <Nav />
+        <Article />
+        <Footer />
     </div>
   );
 }
